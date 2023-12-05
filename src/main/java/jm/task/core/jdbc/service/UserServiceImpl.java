443689889d5fdf766bf50fb897c8507e116b7c4f
SergioDallini <73733633+SergioDallini@users.noreply.github.com>
2023-12-05 23:31:49 +0700

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,40 +1,44 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
-    Util util = new Util();
-    Connection connection = util.getConnection();
+//    UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
+//    Util util = new Util();
+//    Connection connection = util.getConnection();
+UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        daoJDBC.createUsersTable();
+//        daoJDBC.createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
-        daoJDBC.dropUsersTable();
+//        daoJDBC.dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        daoJDBC.saveUser(name, lastName, age);
+//        daoJDBC.saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        daoJDBC.removeUserById(id);
+//        daoJDBC.removeUserById(id);
+        userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return daoJDBC.getAllUsers();
+//        return daoJDBC.getAllUsers();
+        return userDaoHibernate.getAllUsers();
     }
 
 
     public void cleanUsersTable() {
-        daoJDBC.cleanUsersTable();
+//        daoJDBC.cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
     }
 }
